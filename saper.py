61718990cf8ac31fc323abd37f8f044a5727fc25
Emilia Wojtiuk <01158329@pw.edu.pl>
2021-06-05 18:33:04 +0000

    Update saper.py

--- saper.py
+++ saper.py
@@ -1,18 +1,24 @@
 import random
 import json
-URL = {"Board": "/home/emili/paragony/pipr-notes/PIPR/Saper/board.json", "Play_Board": "/home/emili/paragony/pipr-notes/PIPR/Saper/play_board.json"}
+from tkinter.constants import NONE
+URL = {
+    "Board": "/home/emili/paragony/pipr-notes/PIPR/Saper/board.json",
+    "Play_Board": "/home/emili/paragony/pipr-notes/PIPR/Saper/play_board.json"
+    }
 
-oznaczenie_bomb = 9
-lista_obrazkow = ["[ ]", "1", "2", "3", "4", "5", "6", "7", "8", " B ", " F " , " X "]
+oznaczenie_bomb = 11
 
 
 class Dict:
-    def __init__(self, wiersz, koluma, bomby):
+    def __init__(self, wiersz, koluma, bomby, mapa=None):
         self._wiersz = wiersz
         self._koluma = koluma
         self._bomby = bomby
-        self._map = self.make_d()
-        self._map_neigh = self.neighbours()
+        if not mapa:
+            self._map = self.make_d()
+        else:
+            self._map = mapa
+        self.neighbours()
         with open(URL["Board"], "w") as file_with_board:
             json.dump(self._map, file_with_board)
         file_with_board.close()
@@ -65,12 +71,14 @@ class Dict:
 
 
 class Play_board:
-    def __init__(self, wiersz, kolumna, bomby):
+    def __init__(self, wiersz, kolumna, bomby, lista_o=None):
+        self.lista_obrazkow = lista_o
         self._outcome = False
         self._wiersz = wiersz
         self._koluma = kolumna
         self._bomby = bomby
         self._usedF = 0
+        self._win = None
         with open(URL["Board"], "r") as file_with_board:
             self._board = json.load(file_with_board)
         self.make_play_board()
@@ -89,110 +97,117 @@ class Play_board:
         if w >= 0 and w <= self._wiersz-1 and k >= 0 and k <= self._koluma-1:
             return self._new_board[str(w)][k]
 
+    def change_cell(self, w, k, val):
+        if w >= 0 and w <= self._wiersz-1 and k >= 0 and k <= self._koluma-1:
+            self._new_board[str(w)][k] = val
+        return
+
     def make_play_board(self):
         new_board = {}
         for i in range(0, self._wiersz):
             wiersze = []
             for j in range(0, self._koluma):
-                wiersze.append(lista_obrazkow[11])
+                wiersze.append(self.lista_obrazkow[11])
             new_board[i] = wiersze
         with open(URL["Play_Board"], "w") as file_with_board:
             json.dump(new_board, file_with_board)
         return
 
     def click(self, mouse_button, wiersz, kolumna):
-        wartosc_na_miejscu = self.check_cell(wiersz, kolumna)
-        wartosc_na_miejscu_new = self.check_cell_on_new(wiersz, kolumna)
-        if mouse_button == "l" and wartosc_na_miejscu_new != lista_obrazkow[10]:
-            if wartosc_na_miejscu == oznaczenie_bomb:
-                self._outcome = True
-                self.show_bombs()
-            if wartosc_na_miejscu in range(1, 9):
-                self._new_board[str(wiersz)][kolumna] = f'[{lista_obrazkow[wartosc_na_miejscu]}]'
-            if wartosc_na_miejscu == 0:
-                self.odkryj_puste(wiersz, kolumna)
-        if mouse_button == "p" and wartosc_na_miejscu_new == lista_obrazkow[11]:
-            self.change_cell(wiersz, kolumna, lista_obrazkow[10])
-            self._usedF += 1
-        if mouse_button == "p" and wartosc_na_miejscu_new == lista_obrazkow[10]:
-            self.change_cell(wiersz, kolumna, lista_obrazkow[11])
-            self._usedF -= 1
+        if mouse_button == "l":
+            self.left_click(wiersz, kolumna)
+        if mouse_button == "p":
+            self.right_click(wiersz, kolumna)
         with open(URL["Play_Board"], "w") as file_with_board:
             json.dump(self._new_board, file_with_board)
+        if self._outcome is False:
+            self.you_win()
+
+    def left_click(self, wiersz, kolumna):
+        wart_na_miejscu = self.check_cell(wiersz, kolumna)
+        wart_na_new = self.check_cell_on_new(wiersz, kolumna)
+        if wart_na_new != self.lista_obrazkow[10]:
+            if wart_na_miejscu == oznaczenie_bomb:
+                self._outcome = True
+                self.show_bombs()
+            if wart_na_miejscu in range(1, 9):
+                self.change_cell(wiersz, kolumna, self.lista_obrazkow[wart_na_miejscu])
+            if wart_na_miejscu == 0:
+                self.odkryj_puste(wiersz, kolumna)
+
+    def right_click(self, wiersz, kolumna):
+        wartosc_na_miejscu_new = self.check_cell_on_new(wiersz, kolumna)
+        if wartosc_na_miejscu_new == self.lista_obrazkow[11]:
+            self.change_cell(wiersz, kolumna, self.lista_obrazkow[10])
+            self._usedF += 1
+        if wartosc_na_miejscu_new == self.lista_obrazkow[10]:
+            self.change_cell(wiersz, kolumna, self.lista_obrazkow[11])
+            self._usedF -= 1
 
     def show_bombs(self):
         for i in range(0, self._wiersz):
             for j in range(0, self._koluma):
                 if self.check_cell(i, j) == oznaczenie_bomb:
-                    self._new_board[str(i)][j] = lista_obrazkow[9]
-        return
-
-    def change_cell(self, w, k, val):
-        if w >= 0 and w <= self._wiersz-1 and k >= 0 and k <= self._koluma-1:
-            self._new_board[str(w)][k] = val
+                    self.change_cell(i, j, self.lista_obrazkow[9])
+        self._win = False
         return
 
     def odkryj_puste(self, wiersz, kolumna):
-        self._new_board[str(wiersz)][kolumna] = lista_obrazkow[0]
+        self.change_cell(wiersz, kolumna, self.lista_obrazkow[0])
         for i in range(self._koluma*self._wiersz):
             for i in range(0, self._wiersz):
                 for j in range(0, self._koluma):
                     for a in range(-1, 2):
                         for b in range(-1, 2):
-                            if self.check_cell_on_new(i+a, j+b) == lista_obrazkow[0]:
+                            if self.check_cell_on_new(i+a, j+b) == self.lista_obrazkow[0]:
                                 wartosc_na_miejscu = self.check_cell(i, j)
                                 wartosc_na_miejscu_new = self.check_cell_on_new(i, j)
-                                if wartosc_na_miejscu_new != lista_obrazkow[10]:
+                                if wartosc_na_miejscu_new != self.lista_obrazkow[10]:
                                     if wartosc_na_miejscu == 0:
-                                        self.change_cell(i, j, lista_obrazkow[0])
+                                        self.change_cell(i, j, self.lista_obrazkow[0])
                                     else:
-                                        self.change_cell(i, j, f'[{lista_obrazkow[wartosc_na_miejscu]}]')
+                                        self.change_cell(i, j, self.lista_obrazkow[wartosc_na_miejscu])
         return
 
     def you_win(self):
-        x = False
-        if self._bomby == self._usedF:
-            x = True
-            for i in range(0, self._wiersz):
-                for j in range(0, self._koluma):
-                    wartosc_na_miejscu = self.check_cell(i, j)
-                    wartosc_na_miejscu_new = self.check_cell_on_new(i, j)
-                    if wartosc_na_miejscu == oznaczenie_bomb and wartosc_na_miejscu_new != lista_obrazkow[10]:
-                        x = False
-        else:
-            x = True
-            for i in range(0, self._wiersz):
-                for j in range(0, self._koluma):
-                    wartosc_na_miejscu = self.check_cell(i, j)
-                    wartosc_na_miejscu_new = self.check_cell_on_new(i, j)
-                    if wartosc_na_miejscu < oznaczenie_bomb and wartosc_na_miejscu_new == lista_obrazkow[11]:
-                        x = False
-        return x
+        self._outcome = True
+        for i in range(0, self._wiersz):
+            for j in range(0, self._koluma):
+                wartosc_na_miejscu = self.check_cell(i, j)
+                wartosc_new = self.check_cell_on_new(i, j)
+                if wartosc_na_miejscu != oznaczenie_bomb and wartosc_new == self.lista_obrazkow[10]:
+                    self._outcome = False
+                elif wartosc_na_miejscu != oznaczenie_bomb and wartosc_new == self.lista_obrazkow[11]:
+                    self._outcome = False
+        if self._outcome is True:
+            self._win = True
+        return
 
 
 def main():
-    wiersze = 5
-    kolumny = 5
-    bomny = 2
+    lista_obrazkow = [" ", "1", "2", "3", "4", "5", "6", "7", "8", "B", "F", "X"]
+    wiersze = 3
+    kolumny = 3
+    bomny = 1
     mapa = Dict(wiersze, kolumny, bomny)
     mapa.print_map_raw()
-    a = Play_board(wiersze, kolumny, bomny)
+    a = Play_board(wiersze, kolumny, bomny, lista_obrazkow)
     a.print_map_raw()
     while a._outcome is False:
         myszka = input("myszka: ")
-        wiersz = int(input("wiersz: "))-1
-        kolumna = int(input("kolumna: "))-1
+        wiersz = int(input("wiersz: "))
+        kolumna = int(input("kolumna: "))
         if wiersz >= 0 and wiersz <= wiersze and kolumna >= 0 and kolumna <= kolumny:
             a.click(myszka, wiersz, kolumna)
             a.print_map_raw()
-            if a._outcome is True:
+            if a._win is False:
                 print("You loose")
-            if a.you_win() is True:
+            if a._win is True:
                 print("You win")
-                a._outcome = True
         else:
             print("Wrong input try again")
 
 
 if __name__ == "__main__":
     main()
+
